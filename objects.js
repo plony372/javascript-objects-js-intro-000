53de4ff037f7713d592c71872ac6de8a8c60c705
plony benav <menachemfuterfas@gmail.com>
2021-02-18 15:53:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {'avraham freid': 'father dont crie'}
 
 
 updatePlaylist(playlist, artistName, songTitle){
-  playlist['artistName'] = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
