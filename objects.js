e4554866acb000a2b5f3a92783dd44eff089898b
plony benav <menachemfuterfas@gmail.com>
2021-02-18 16:10:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
+  var newlist = delete playlist.artistName;
+  return newlist
   
 }
